9ac3de382398f423cb709ba23b58762d3bb5a4ae
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-02 18:44:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    while
+    if array[index] == value_to_find
+      return array[index]
+    end
+  end
 end
 
 def find_max_value(array)
